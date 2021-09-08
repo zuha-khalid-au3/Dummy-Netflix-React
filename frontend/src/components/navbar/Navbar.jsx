43ae6aa2636b1f94b6077e3f58e734ee59441555
zuha-khalid-au3 <zuhakhalid@gmail.com>
2021-09-08 12:41:57 +0530

    Featured Implementation

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,11 +1,21 @@
-import "./navbar.scss";
+import "./Navbar.scss";
 import { Search, Notifications, ArrowDropDown } from "@material-ui/icons";
+import { useState } from "react";
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  console.log(isScrolled);
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
-          <img src="" alt="Logo" />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
           <span>Homepage</span>
           <span>Series</span>
           <span>Movies</span>
